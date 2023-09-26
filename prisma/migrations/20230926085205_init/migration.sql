1957b5febb65e3a413eccf2c1322baf551e120e3
sam <samgus02@edu.umea.se>
2023-09-26 11:33:36 +0200

    balls

---
+++ prisma/migrations/20230926085205_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_name_key" ON "user"("name");
